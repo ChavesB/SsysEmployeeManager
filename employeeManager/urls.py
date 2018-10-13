8f16fa9727cfb1748d72e1f0d7f136c2a5729b97
ChavesB <brucosstta@gmail.com>
2018-10-13 18:11:17 -0400

    Adicionado a parte da api django com as funções: listar, adicionar e remover

--- employeeManager/urls.py
+++ employeeManager/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employeesList, name='employeeList'),
+    path('', views.employeesList, name='employeesList'),
     path('employee/<int:pk>/edit/', views.employeeEdit, name='employeeEdit'),
     path('employee/<int:pk>/delete/', views.employeeDelete, name='employeeDelete'),
     path('employee/new', views.employeeNew, name='employeeNew'),
+    path('employee/', views.employeeList, name='list'),
+    path('employee/add/', views.employeeAdd, name='add'),
+    path('employee/remove/', views.employeeRemove, name='remove'),
 ]
--- employeeManager/views.py
+++ employeeManager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse,HttpResponse
 from .models import Employee
 from .forms import EmployeeForm
+import json
 
 def employeesList(request):
     employees = Employee.objects.all()
@@ -31,3 +33,32 @@ def employeeNew(request):
     else:
         form = EmployeeForm()
     return render(request, 'employee/employeeEdit.html', { 'form': form })
+
+
+#API DJANGO com as funcionalidades listar, adicionar e remover
+from django.views.decorators.csrf import csrf_exempt
+
+def employeeList(request):
+    resp = [{ 'name':employee.name, 'email': employee.email, 'department': employee.department } for employee in Employee.objects.all()]
+    resp_json = json.dumps(resp)
+    return HttpResponse(resp_json, content_type='application/json')
+
+@csrf_exempt
+def employeeAdd(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'add': 'success'})
+    return JsonResponse({'add': 'failed'})
+
+@csrf_exempt
+def employeeRemove(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            employee = form.save(commit=False)
+            employee = Employee.objects.get(name=employee.name, email=employee.email, department=employee.department)
+            employee.delete()
+            return JsonResponse({'remove': 'success'})
+    return JsonResponse({'remove': 'failed'})
